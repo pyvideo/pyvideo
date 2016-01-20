44ad4fe909e52ebd913700b15e98c2d3afe16836
Paul Logston <code@logston.me>
2016-01-20 00:58:26 -0500

    Start on theme pytube-201601

---
+++ content/pygotham/test.rst
@@ -0,0 +1,11 @@
+My super title
+##############
+
+:date: 2010-10-03 10:20
+:tags: thats, awesome
+:category: PyGotham 2016
+:slug: my-super-post
+:authors: Alexis Metaireau, Conan Doyle
+:summary: Short version for index and feeds
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Paul Logston'
+SITENAME = 'pytube'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+DELETE_OUTPUT_DIRECTORY = True
+
+THEME = 'themes/pytube-201601/'
+
