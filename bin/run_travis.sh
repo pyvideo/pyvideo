7cfa1d1cb9bb079e9c20e9014d0893ddc3224978
Paul Logston <code@logston.me>
2016-02-05 19:57:02 -0500

    Here we go again

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -3,7 +3,7 @@
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     git config --global user.email "code@logston.me"
     git config --global user.name "Paul Logston"
-    export GIT_SSH=bin/ssh-git.sh
+    export GIT_SSH=../bin/ssh-git.sh
     export PKEY=.travis.priv
     make content && make deploy
 else
