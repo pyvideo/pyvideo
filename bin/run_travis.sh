8bba44500717eefab93eba76478282e559279853
Paul Logston <code@logston.me>
2016-02-05 20:00:50 -0500

    Here comes the sun, do de doo doo

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -4,7 +4,7 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     git config --global user.email "code@logston.me"
     git config --global user.name "Paul Logston"
     export GIT_SSH=../bin/ssh-git.sh
-    export PKEY=.travis.priv
+    export PKEY=../.travis.priv
     make content && make deploy
 else
     make content && make html
