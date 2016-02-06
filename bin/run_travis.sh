bfb6aa9e9976bee2fa2a9f43f68f4ac5b0ea8c0c
Paul Logston <code@logston.me>
2016-02-05 19:51:57 -0500

    Another shot at encryption and travis

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    #git config --global user.email "code@logston.me"
-    #git config --global user.name "Paul Logston"
+    git config --global user.email "code@logston.me"
+    git config --global user.name "Paul Logston"
     export GIT_SSH=bin/ssh-git.sh
     export PKEY=.travis.priv
     make content && make deploy
