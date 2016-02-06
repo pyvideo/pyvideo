9da96b7c98f1539992e88e47ccb17854af1051cd
Paul Logston <code@logston.me>
2016-02-05 20:34:02 -0500

    Add deploy key for pytube.github.io

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+    chmod 600 travis
     git config --global user.email "code@logston.me"
     git config --global user.name "Paul Logston"
     export GIT_SSH=../bin/ssh-git.sh
-    export PKEY=../.travis.priv
-    chmod 600 .travis.priv
+    export PKEY=../travis
     make content && make deploy
 else
     make content && make html
