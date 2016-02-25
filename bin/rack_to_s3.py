4687eae052c1796f67f496a13aaf224e4bec75d7
Paul Logston <code@logston.me>
2016-02-25 03:30:35 +0000

    Use boto / initiate_multipart_upload for upload

--- bin/rack_to_s3.py
+++ bin/rack_to_s3.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import glob
 import json
+import math
 import os
 from urllib.parse import urlparse
 from urllib.request import urlopen
@@ -8,6 +9,7 @@ import sys
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+from filechunkio import FileChunkIO
 
 
 DATA_DIR = 'data'
@@ -19,6 +21,8 @@ KEYS = (
 )
 PYTUBE_BUCKET = 'pytube'
 AWSID, AWSSECRET = os.environ.get('AWSID'), os.environ.get('AWSSECRET')
+UP_CHUNK_SIZE = 52428800
+TEMP_FILE = 'temp-media-file'
 
 
 def generate_paths_to_copy(json_file_paths):
@@ -39,14 +43,44 @@ def generate_paths_to_copy(json_file_paths):
 
 def copy(args):
     bucket, source, dest = args
-    sys.stdout.write('Working on: {}\n'.format(source))
+    sys.stdout.write('Downloading: {}\n'.format(source))
     sys.stdout.flush()
-    response = urlopen(source)
+    
+    try:
+        response = urlopen(source)
+        headers = {'Content-Type' : response.info().get_content_type()}
+        with open(TEMP_FILE, 'wb') as fp:
+            while True:
+                chunk = response.read(16 * 1024)
+                if not chunk:
+                    break
+                fp.write(chunk)
+    except Exception as e:
+        sys.stdout.write('Failed to download: {}\n'.format(source))
+        sys.stdout.write('{}\n'.format(e.message))
+        sys.stdout.flush()
+        return
 
-    headers = {'Content-Type' : response.info().get_content_type()}
-    k = Key(bucket)
-    k.name = dest
-    k.set_contents_from_string(response.read(), headers)
+    source_size = os.stat(TEMP_FILE).st_size
+    mp = bucket.initiate_multipart_upload(dest, headers=headers, reduced_redundancy=True)
+    chunk_count = int(math.ceil(source_size / float(UP_CHUNK_SIZE)))
+
+    try:
+        sys.stdout.write('Uploading: {}\n'.format(source))
+        sys.stdout.flush()
+        for i in range(chunk_count):
+            offset = UP_CHUNK_SIZE * i
+            bytes = min(UP_CHUNK_SIZE, source_size - offset)
+            with FileChunkIO(TEMP_FILE, 'r', offset=offset, bytes=bytes) as fp:
+                mp.upload_part_from_file(fp, part_num=i + 1)
+
+        mp.complete_upload()
+    except Exception as e:
+        mp.cancel_upload()
+        sys.stdout.write('Failed to upload: {}\n'.format(source))
+        sys.stdout.write('{}\n'.format(e))
+        sys.stdout.flush()
+        return
 
 
 def main():
@@ -59,10 +93,8 @@ def main():
 
     args = ((bucket, path, urlparse(path).path) for path in paths)
 
-    with concurrent.futures.ThreadPoolExecutor() as p:
-        futures = p.map(copy, args)
-        for future in concurrent.futures.as_completed(futures):
-            future.result()  # allow exceptions to percolate up
+    for arg_tup in args:
+       copy(arg_tup) 
 
     print('Done.')
 
