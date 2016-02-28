9d7a045e227c10159e0c9ff995213348b8e828e3
Paul Logston <code@logston.me>
2016-02-28 02:49:16 -0500

    Add JS for publishing content

---
+++ themes/pytube-201601/static/js/publish.js
@@ -0,0 +1,30 @@
+$('#js-publish-btn').click(function onClick() {
+  "use strict";
+
+  var url = 'http://publisher.pytube.org/publish';
+
+  // for testing
+  var url = 'http://publisher.pytube.vg/publish';
+
+  var data = {
+    data_file: $('#data_file').val(),
+    category_slug: $('#category_slug').val(),
+    media_record_slug: $('#media_record_slug').val(),
+    publish_key: $('#publish_key').val()
+  };
+
+  $.post({
+    url: url,
+    data: JSON.stringify(data),
+    dataType: 'application/json'
+  }).done(function onDone(res) {
+    if (res.result === 'ok') {
+      alert('Publish request recieved!');
+    } else if (res.result === 'failure') {
+      alert('Publish request failed: ' + res.msg);
+    }
+  }).fail(function onFail() {
+    alert('Communication error. Please try again another time.');
+  });
+});
+
