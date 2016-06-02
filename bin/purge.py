eef89555ebb5449baaf626e1fb66fbbae2876e1c
Paul Logston <code@logston.me>
2016-06-02 15:40:44 -0700

    Update purge command to accept args

--- bin/purge.py
+++ bin/purge.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 
 CONTENT_DIR = 'content'
@@ -8,8 +9,13 @@ CONTENT_DIR_KEEP = set(('pages', 'images', 'extra'))
 
 def purge_content():
     contents = os.listdir(CONTENT_DIR)
+
+    dirs_to_keep = CONTENT_DIR_KEEP.copy()
+    if sys.argv[1].strip():
+        dirs = (dir_.strip() for dir_ in sys.argv[1].strip().split(','))
+        dirs_to_keep.update(dirs)
     for dir_ in contents:
-        if dir_ not in CONTENT_DIR_KEEP:
+        if dir_ not in dirs_to_keep:
             shutil.rmtree(os.path.join(CONTENT_DIR, dir_))
 
 
