99d4516ee46df17d5c16b638dced91645fe5660c
Paul Logston <code@logston.me>
2016-06-05 21:05:09 -0400

    Reorganize travis CI build script

--- .travis.sh
+++ .travis.sh
@@ -8,7 +8,7 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     git config --global user.email "code@logston.me"
     git config --global user.name "Paul Logston"
     # Set location of GIT_SSH key relative to inside output/
-    export GIT_SSH=../bin/ssh-git.sh
+    export GIT_SSH=../.ssh/ssh-git.sh
     # Set location of deploy key relative to inside output/
     export DEPLOY_KEY=../.ssh/travis
     make deploy
