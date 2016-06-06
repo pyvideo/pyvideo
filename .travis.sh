047b1bd46a9fd66561ebca6c0431ce9e47edf22b
Paul Logston <code@logston.me>
2016-06-05 21:41:38 -0400

    Prep travis.sh for preview env

--- .travis.sh
+++ .travis.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
+# Set correct permissions on deploy key
+chmod 600 .ssh/travis
+# Set global configs for author of repo
+git config --global user.email "code@logston.me"
+git config --global user.name "Paul Logston"
+# Set location of GIT_SSH key relative to inside output/
+export GIT_SSH=../.ssh/ssh-git.sh
+# Set location of deploy key relative to inside output/
+export DEPLOY_KEY=../.ssh/travis
+
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    echo "Building and deploying";
-    # Set correct permissions on deploy key
-    chmod 600 .ssh/travis
-    # Set global configs for author of repo
-    git config --global user.email "code@logston.me"
-    git config --global user.name "Paul Logston"
-    # Set location of GIT_SSH key relative to inside output/
-    export GIT_SSH=../.ssh/ssh-git.sh
-    # Set location of deploy key relative to inside output/
-    export DEPLOY_KEY=../.ssh/travis
+    echo "Building and deploying to production";
     make deploy
 else
     echo "Building PR ${TRAVIS_PULL_REQUEST}";
-    make html
+    make deploy-preview
 fi
 
