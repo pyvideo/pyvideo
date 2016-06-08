1f73126c93e4055c00e31d757210037be8a442d5
Paul Logston <code@logston.me>
2016-06-08 02:46:46 -0400

    Add plugin to avoid publishing certain articles

---
+++ plugins/drop_no_publish.py
@@ -0,0 +1,50 @@
+import glob
+import json
+import os
+
+from pelican import signals
+
+
+def drop_no_publish(pelican):
+    """
+    Update ARTICLE_EXCLUDES in pelicanconf with list of articles
+    that should be excluded based on their ID. The list of article
+    IDs that should be dropped is located in NO_PUBLISH_FILE.
+    """
+    excludes = pelican.settings.get('ARTICLE_EXCLUDES') or []
+    path = pelican.settings.get('PATH')
+    data_dir = pelican.settings.get('DATA_DIR')
+
+    no_publish_file = pelican.settings.get('NO_PUBLISH_FILE')
+    if not no_publish_file:
+        return
+
+    with open(os.path.join(path, data_dir, no_publish_file)) as fp:
+        no_publish_ids = set(json.load(fp))
+
+    if not no_publish_ids:
+        return
+
+    paths = get_no_publish_paths(path, no_publish_ids)
+
+    pelican.settings['ARTICLE_EXCLUDES'] = excludes + paths
+
+
+def get_no_publish_paths(pelican_path, no_publish_ids):
+    search_pattern = os.path.join(pelican_path, '**/**/**/*.json')
+
+    no_publish_paths = []
+    for file_path in glob.iglob(search_pattern):
+        with open(file_path) as fp:
+            blob = json.load(fp)
+            if isinstance(blob, dict):
+                file_id = blob.get('id')
+                if file_id in no_publish_ids:
+                    no_publish_paths.append(file_path)
+
+    return no_publish_paths
+
+
+def register():
+    signals.initialized.connect(drop_no_publish)
+
