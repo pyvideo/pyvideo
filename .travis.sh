028d8b8cad97f23fbf62badbaa6bced7ead00518
Paul Logston <code@logston.me>
2016-08-20 15:54:54 -0400

    Update deploy key location

--- .travis.sh
+++ .travis.sh
@@ -6,24 +6,19 @@ git config --global user.name "Paul Logston"
 # Set location of GIT_SSH key relative to inside output/
 export GIT_SSH=../.ssh/ssh-git.sh
 
-# Set correct permissions on deploy key
-chmod 600 .ssh/travis
-# Set location of deploy key relative to inside output/
-export DEPLOY_KEY=../.ssh/travis
-
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     echo "Building and deploying to production";
     # Set correct permissions on deploy key
-    chmod 600 .ssh/travis
+    chmod 600 keys/production
     # Set location of deploy key relative to inside output/
-    export DEPLOY_KEY=../.ssh/travis
+    export DEPLOY_KEY=../keys/production
     make deploy
 else
     echo "Building PR ${TRAVIS_PULL_REQUEST}";
     # Set correct permissions on deploy key
-    chmod 600 .ssh/preview
+    chmod 600 keys/preview
     # Set location of deploy key relative to inside output/
-    export DEPLOY_KEY=../.ssh/preview
+    export DEPLOY_KEY=../keys/preview
     make deploy-preview
 fi
 
