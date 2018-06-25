2763961dd5a104e387a14bf1446ba26296e2f210
Paul Logston <code@logston.me>
2018-06-25 13:16:36 -0400

    Add a script to generate and store random links

---
+++ run_random_links.sh
@@ -0,0 +1,8 @@
+# This script is used to generate and store "used links" 
+# that are distributed by the PSF.
+git pull
+python tools/random_link.py tools/used_links.txt
+git add tools/used_links.txt
+git commit -m "Add used links"
+git push origin HEAD
+
