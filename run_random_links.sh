cebf69a3d14a60d466e6975facb4b9502a7a223b
Paul Logston <paul.logston@gmail.com>
2019-10-28 11:10:41 -0400

    Update Run Random Links

--- run_random_links.sh
+++ run_random_links.sh
@@ -1,5 +1,8 @@
 # This script is used to generate and store "used links" 
 # that are distributed by the PSF.
+cd data
+git pull
+cd ..
 git pull
 python tools/random_link.py tools/used_links.txt
 git add tools/used_links.txt
