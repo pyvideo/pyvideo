2ddf02fbec86d61bc960a5c64ba5de15431037b0
Jon Banafato <jon@jonafato.com>
2025-09-20 18:50:01 -0400

    Upgrade Pelican from 3.7.1 to 4.0.1
    Upgrade Pelican from the 3.x series to the 4.x series. This upgrade
    unblocks other dependency upgrades and a Python version upgrade. Changes
    include:
    
    1. The Pelican dependency version
    
    2. URL and slug handling logic as recommended by the [changelog]
    
    3. Overriding the `PyVideoArticleGenerator.get_files` method to preserve
       Pelican's pre-4.0 behavior to resolve tag rendering differences
       between two PyVideo builds
    
    The output of these two versions has been compared using [difftastic]
    and found to be syntactically identical, so there should be no end-user
    differences present here.
    
    [changelog]: https://docs.getpelican.com/en/4.0.1/changelog.html#id1
    [difftastic]: https://difftastic.wilfred.me.uk/

--- plugins/drop_no_publish.py
+++ plugins/drop_no_publish.py
@@ -5,10 +5,12 @@ import os
 
 import pelican
 from pelican import signals
+import six
 
 
-# Monkey patch the ArticleGenerator to override specific method.
-# We have to override the `_include_path` method because it does not
+# Monkey patch the ArticleGenerator to override two specific methods.
+# 
+# 1. We have to override the `_include_path` method because it does not
 # filter files with enough specificity. If we wanted to filter a talk
 # at `categories/jacksconf/videos/jacks-talk.json`, we could do so
 # (without using this patch) by including `jacks-talk.json` in the
@@ -20,6 +22,17 @@ from pelican import signals
 # level.
 # Hopefully this patch can be removed by the merger of this PR:
 # https://github.com/getpelican/pelican/pull/1975
+#
+# 2. Override the `get_files` method to use the pre-4.0.0 behavior. This is
+# required to maintain consistent rendering with the behavior of PyVideo when
+# built using Pelican 3.7.1. In Pelican >=4.0.0, the `files` variable in this
+# method is a set rather than a list, which manifests as PyVideo tags
+# seemingly changing their case and formatting between renders due to file
+# ordering and name collisions after the tag names are run through Pelican's
+# `slugify` process. As a next step to remove this method override, the data
+# repository should be updated to ensure consistency with tags (e.g. "Data
+# Science" vs. "data science" vs. "data-science"), and a check should be added
+# to prevent future tag duplication.
 class PyVideoArticlesGenerator(pelican.ArticlesGenerator):
     def _include_path(self, path, extensions=None):
         """Inclusion logic for .get_files(), returns True/False
@@ -43,6 +56,49 @@ class PyVideoArticlesGenerator(pelican.ArticlesGenerator):
         if extensions is False or basename.endswith(extensions):
             return True
         return False
+
+    def get_files(self, paths, exclude=[], extensions=None):
+        """Return a list of files to use, based on rules
+        :param paths: the list pf paths to search (relative to self.path)
+        :param exclude: the list of path to exclude
+        :param extensions: the list of allowed extensions (if False, all
+            extensions are allowed)
+        """
+        # backward compatibility for older generators
+        if isinstance(paths, six.string_types):
+            paths = [paths]
+        # group the exclude dir names by parent path, for use with os.walk()
+        exclusions_by_dirpath = {}
+        for e in exclude:
+            parent_path, subdir = os.path.split(os.path.join(self.path, e))
+            exclusions_by_dirpath.setdefault(parent_path, set()).add(subdir)
+
+        files = []
+        ignores = self.settings['IGNORE_FILES']
+        for path in paths:
+            # careful: os.path.join() will add a slash when path == ''.
+            root = os.path.join(self.path, path) if path else self.path
+            if os.path.isdir(root):
+                for dirpath, dirs, temp_files in os.walk(
+                        root, followlinks=True):
+                    drop = []
+                    excl = exclusions_by_dirpath.get(dirpath, ())
+                    for d in dirs:
+                        if (d in excl or
+                            any(fnmatch.fnmatch(d, ignore)
+                                for ignore in ignores)):
+                            drop.append(d)
+                    for d in drop:
+                        dirs.remove(d)
+                    reldir = os.path.relpath(dirpath, self.path)
+                    for f in temp_files:
+                        fp = os.path.join(reldir, f)
+                        if self._include_path(fp, extensions):
+                            files.append(fp)
+            elif os.path.exists(root) and self._include_path(path, extensions):
+                files.append(path)  # can't walk non-directories
+        return files
+
 pelican.ArticlesGenerator = PyVideoArticlesGenerator
 
 
